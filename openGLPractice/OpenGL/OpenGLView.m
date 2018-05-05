36474e3f62d1481723c10a78406dda599eeb70f1
Booharin <booharin@bk.ru>
2018-05-05 14:12:27 +0300

    Added shader for rendering background gradient view

--- openGLPractice/OpenGL/OpenGLView.m
+++ openGLPractice/OpenGL/OpenGLView.m
@@ -10,6 +10,25 @@
 
 @implementation OpenGLView
 
+typedef struct {
+    float Position[3];
+    float Color[4];
+} Vertex;
+
+const Vertex Vertices[] = {
+    {{1, -1, 0}, {1, 0, 0, 1}},
+    {{1, 1, 0}, {0, 1, 0, 1}},
+    {{-1, 1, 0}, {0, 0, 1, 1}},
+    {{-1, -1, 0}, {0, 0, 0, 1}}
+};
+
+const GLubyte Indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
+
+//MARK: - Compiling shaders from source with type - vertex or fragment
+
 - (GLuint)compileShader: (NSString*)shaderName withType:(GLenum)shaderType {
     
     NSString* shaderPath = [[NSBundle mainBundle] pathForResource:shaderName
@@ -48,12 +67,14 @@
     return shaderHandle;
 }
 
+//MARK: - Linking shaders together and telling OpenGL use this program
+
 - (void)compileShaders {
     GLuint vertexShader = [self compileShader:@"SimpleVertex"
                                      withType:GL_VERTEX_SHADER];
     GLuint fragmentShader = [self compileShader:@"SimpleFragment"
                                        withType:GL_FRAGMENT_SHADER];
-    GLuint programHandle = glCreateProgram;
+    GLuint programHandle = glCreateProgram();
     glAttachShader(programHandle, vertexShader);
     glAttachShader(programHandle, fragmentShader);
     glLinkProgram(programHandle);
@@ -79,6 +100,18 @@
     glEnableVertexAttribArray(_colorSlot);
 }
 
+- (void)setupVBOs {
+    GLuint vertexBuffer;
+    glGenBuffers(1, &vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertices), Vertices, GL_STATIC_DRAW);
+    
+    GLuint indexBuffer;
+    glGenBuffers(1, &indexBuffer);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBuffer);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(Indices), Indices, GL_STATIC_DRAW);
+}
+
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
@@ -87,6 +120,7 @@
         [self setupRenderBuffer];
         [self setupFrameBuffer];
         [self compileShaders];
+        [self setupVBOs];
         [self render];
     }
     return self;
@@ -145,6 +179,23 @@
 - (void)render {
     glClearColor(48.0/255.0, 54.0/255.0, 92.0/255.0, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
+    
+    glViewport(0, 0, self.frame.size.width, self.frame.size.height);
+    
+    glVertexAttribPointer(_positionSlot,
+                          3,
+                          GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex),
+                          0);
+    glVertexAttribPointer(_colorSlot,
+                          4,
+                          GL_FLOAT, GL_FALSE,
+                          sizeof(Vertex),
+                          (GLvoid*) (sizeof(float) * 3));
+    
+    glDrawElements(GL_TRIANGLES, sizeof(Indices)/sizeof(Indices[0]),
+                   GL_UNSIGNED_BYTE, 0);
+    
     [_context presentRenderbuffer:GL_RENDERBUFFER];
 }
 
